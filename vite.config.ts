83453c1eb76b32f1b69d0b99240d01b65c0aefc7
Ravi Kumar <ravi@Ravis-MacBook-Air.local>
2025-09-07 01:15:48 +0530

    RAVI

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/devforges.github.io/',   // 👈 this must be your repo name
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
